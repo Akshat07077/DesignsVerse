91009e2b8a96a6d22cc5ce34198fc1fccd72ebfd
Akshat Sharma <messageakshat@gmail.com>
2025-02-08 23:16:12 +0530

    Update brandsData.tsx

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -3,10 +3,10 @@ import { Brand } from "@/types/brand";
 const brandsData: Brand[] = [
   { id: 6, name: "Winning Award", number: "50+", /*href: "https://formbold.com" */},
   { id: 1, name: "Global Brands", number: "22+", /*href: "https://uideck.com" */},
-  { id: 2, name: "Happy Client",  number: "60+", /*href: "https://tailgrids.com" */},
-  { id: 3, name: "Happy Clients", number: "10+", /* href: "https://lineicons.com"*/ },
-  { id: 4, name: "Winning Award", number: "45+", /*href: "https://tailadmin.com" */},
-  { id: 5, name: "Happy Clienta", number: "96+", /*href: "https://plainadmin.com"*/ },
+  { id: 2, name: "Happy Clients",  number: "60+", /*href: "https://tailgrids.com" */},
+  { id: 3, name: "Projects Niche", number: "10+", /* href: "https://lineicons.com"*/ },
+  { id: 4, name: "Impressions/Month", number: "455K+", /*href: "https://tailadmin.com" */},
+  { id: 5, name: "Happy Clients", number: "96+", /*href: "https://plainadmin.com"*/ },
 ];
 
 export default brandsData;
