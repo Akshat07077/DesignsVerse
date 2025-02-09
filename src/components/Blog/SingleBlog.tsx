7fe455c4975ea7e162bc7a7c4e3c61e5a354d8f9
Akshat Sharma <messageakshat@gmail.com>
2025-02-09 20:38:19 +0530

    Update SingleBlog.tsx

--- src/components/Blog/SingleBlog.tsx
+++ src/components/Blog/SingleBlog.tsx
@@ -3,15 +3,26 @@ import Image from "next/image";
 import Link from "next/link";
 
 const SingleBlog = ({ blog }: { blog: Blog }) => {
-  const { id, title, image, paragraph, author, tags, publishDate } = blog;
+  const { id, title, video, paragraph, author, tags, publishDate } = blog;
 
   return (
-    <div className="group relative overflow-hidden rounded-sm bg-white shadow-one duration-300 hover:shadow-two dark:bg-dark dark:hover:shadow-gray-dark">
+    <div className="group mb-10 relative overflow-hidden  bg-white shadow-one duration-300 hover:shadow-two dark:bg-dark dark:hover:shadow-gray-dark">
       <Link href={`/blog/${id}`} className="relative block aspect-[37/22] w-full">
         <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white">
           {tags[0]}
         </span>
-        <Image src={image} alt="image" fill />
+        {video ? (
+          <video
+            src={video}
+            autoPlay
+            loop
+            muted
+            className="object-cover w-full h-full"
+          />
+          ) : (
+            <Image src={author.image} alt="author" fill className="object-cover rounded-full" />
+          )}
+
       </Link>
       <div className="p-6 sm:p-8 md:px-6 md:py-8 lg:p-8 xl:px-5 xl:py-8 2xl:p-8">
         <h3>
