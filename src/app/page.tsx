470d804503d5cec07eeb0a9e1c9487d3097080e6
harshmalviya2003 <harshmalviya343@gmail.com>
2025-05-28 13:48:16 +0530

    #upa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
       <MobileServices/>
       <Brands />
       {/* <Features /> */}
-      <RecentProjects/>
+      {/* <RecentProjects/> */}
    
       <AboutSectionOne />
       <AboutSectionTwo />
